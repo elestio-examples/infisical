c88da26ca4ba332ac86a8cde11cb8c2517977f12
Prem Chavhan <104148610+premchavhan99@users.noreply.github.com>
2024-01-30 16:47:03 +0530

    Update build.sh

--- tests/frontend/build.sh
+++ tests/frontend/build.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 rm -rf ./nginx/
 mv nginx-new/ nginx/
+rm -f ./frontend/README.md
 cp -r ./frontend/* ./
 docker buildx build . --output type=docker,name=elestio4test/infisical-frontend:latest | docker load
